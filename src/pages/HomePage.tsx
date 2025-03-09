ab4c3ee174619f90e6547008b7d726d45e7ed5e6
lindstrom.elle@gmail.com <lindstrom.elle@gmail.com>
2025-03-09 20:45:00 +0100

    clean up homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,14 +4,10 @@ const HomePage: React.FC = () => {
   return (
     <>
       <h1 className="press-start-2p-regular text-center">
-        Welcome to the Galactic Encyclopedia of Star Wars Wonders
+      Welcome to the Galactic Encyclopedia
       </h1>
       <h2 className="text-center">Explore the Galaxy Far, Far Away</h2>
       <div className="soldier"></div>
-      <h4 className="text-center">
-        And yes, that stormtrooper is indeed moonwalking. Because in our galaxy,
-        even the Empire likes to have a little fun.
-      </h4>
       <p className="press-start-2p-regular text-center">May the Force (and the funk) be with you!</p>
     </>
   );
