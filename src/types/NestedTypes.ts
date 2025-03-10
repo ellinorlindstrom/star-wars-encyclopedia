07b1310c7ddc3d0a9abc98c023d704f7171d8388
lindstrom.elle@gmail.com <lindstrom.elle@gmail.com>
2025-03-10 20:32:27 +0100

    simplify nested types

--- src/types/NestedTypes.ts
+++ src/types/NestedTypes.ts
@@ -1,24 +1,10 @@
-export interface Character {
+export interface Entity {
     id: number;
     name: string;
 }
 
-export interface Planet {
-    id: number;
-    name: string;
-}
-
-export interface Starship {
-    id: number;
-    name: string;
-}
-
-export interface Vehicle {
-    id: number;
-    name: string;
-}
-
-export interface Species {
-    id: number;
-    name: string;
-}
+export type Character = Entity;
+export type Planet = Entity;
+export type Starship = Entity;
+export type Vehicle = Entity;
+export type Species = Entity;
